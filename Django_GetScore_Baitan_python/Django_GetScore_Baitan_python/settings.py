ce93416ed3e89770a1f0ca0bda272da2057efd25
FnEsc <1033742389@qq.com>
2020-08-10 11:23:14 +0800

    Update settings.py
    secret key hidden

--- Django_GetScore_Baitan_python/Django_GetScore_Baitan_python/settings.py
+++ Django_GetScore_Baitan_python/Django_GetScore_Baitan_python/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_ssq$!nf7x7jp@*+8o5upoek8h_ds7*k!r46d0(q3f-bnd5s7-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
