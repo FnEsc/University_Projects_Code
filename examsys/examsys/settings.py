69106ed3faf21256c4f42364c722e52be3091647
FnEsc <1033742389@qq.com>
2020-08-10 11:25:01 +0800

    Update settings.py
    secret key hidden

--- examsys/examsys/settings.py
+++ examsys/examsys/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yjfuz(a@s)#=0%s-m%gy_=(+((ajxn&6#1j+s181a^7imuwmj9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
